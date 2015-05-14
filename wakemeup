2a6868cbe4bf56319509c88c825d676bb37ab362
jddunn <johnnyddunn@gmail.com>
2015-05-14 19:29:08 -0400

    sdf

---
+++ wakemeup
@@ -0,0 +1,6 @@
+<script language="javascript" type="text/javascript">
+ function playSound(soundfile) {
+ document.getElementById("dummy").innerHTML=
+ "<embed src=\""+soundfile+"\" hidden=\"true\" autostart=\"true\" loop=\"false\" />";
+ }
+ </script>
---
+++ wakemeup.js
@@ -0,0 +1,16 @@
+function mouseDown() {
+ 	    document.getElementById("dreamCatcher").style.color = "#9C9C9C";
+ 	    document.body.style.background = "white";
+ }
+
+function mouseUp() {
+ 		document.body.style.background = "yellow";
+		setTimeout(function(){document.body.style.background="white"},250);
+ }
+
+function myTimer() {
+    var d = new Date();
+    document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+}
+
+}
